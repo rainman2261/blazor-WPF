33f1eb9bf013e87a514ed0eaeb455b83474a9a31
rainman2261 <154292310+rainman2261@users.noreply.github.com>
2024-01-18 08:53:55 +1100

    setup solution

---
+++ nexus.plugins.media/wwwroot/script/winUIBlazorInterop.js
@@ -0,0 +1,18 @@
+﻿var interopFunctions = {};
+
+interopFunctions.registerIndexComponent = (dotnetObj) => {
+  interopFunctions.indexComponent = dotnetObj;
+};
+
+interopFunctions.setFirstName = (firstName) => {
+  console.log("JavaScript setFirstName executed");
+
+  interopFunctions.indexComponent.invokeMethodAsync('SetFirstName', firstName);
+};
+
+interopFunctions.setFirstNameInWinUI = (firstName) => {
+  window.chrome.webview.postMessage(JSON.stringify({
+    messageType: 'firstName',
+    value: firstName
+  }));
+};
